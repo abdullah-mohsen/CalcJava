05a92c5f29427a0c73871c117b2fd25f15b6e1b3
abdullah-mohsen <abdullahhaa144@gmail.com>
2016-06-27 12:38:44 +0300

    frist

---
+++ ButtonHandler.java
@@ -0,0 +1,83 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JTextField;
+public class ButtonHandler implements ActionListener
+{
+		double firstNumber , secondNumber;
+		char math='a';   //test first n or second n
+		public void actionPerformed( ActionEvent e )
+		{
+			JTextField result = Calculator.result;
+			
+			char str= e.getActionCommand().charAt(0);
+			switch(str)
+			{
+				case '0':
+				case '1':
+				case '2':
+				case '3':
+				case '4':
+				case '5':
+				case '6':
+				case '7':
+				case '8':
+				case '9':
+				case '.':
+					
+				result.setText(result.getText() + str);
+				if(math == 'a')
+				firstNumber = Double.valueOf(result.getText()).doubleValue();
+				else
+				secondNumber = Double.valueOf(result.getText()).doubleValue();
+				break;
+				case 'c':
+				result.setText("");
+				firstNumber=0;
+				secondNumber=0;
+				break;	
+			}
+			if(e.getSource() == Calculator.add)
+			{
+			result.setText("");
+			math='+';
+			}
+			else if(e.getSource() == Calculator.sub)
+			{
+			result.setText("");
+			math='-';
+			}
+			else if(e.getSource() == Calculator.mult)
+			{
+			result.setText("");
+			math='*';
+			}
+			else if(e.getSource() == Calculator.div)
+			{
+			result.setText("");
+			math='/';
+			}
+			else if(e.getSource() == Calculator.equal)
+			{
+			double p=0.0;
+			switch(math){
+			case'+':
+			p=firstNumber+secondNumber;
+			break;
+			case'-':
+			p=firstNumber-secondNumber;
+			break;
+			case'*':
+			p=firstNumber*secondNumber;
+			break;
+			case'/':
+			p=firstNumber/secondNumber;
+			break;
+			}
+			result.setText(String.valueOf(p));
+			math='a';
+			firstNumber=0;
+			secondNumber=0;
+		}
+		}
+		}// class button handler
---
+++ Calculator.java
@@ -0,0 +1,56 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class Calculator extends JFrame {
+		public static JButton add,sub,div,mult,equal , clear;
+		public static JTextField result;
+		JButton arbt[];
+		public Calculator()
+		{
+			super("calculator");
+		JPanel panel = new JPanel();
+		panel.setLayout( new FlowLayout());
+		result=new JTextField("",12);
+		panel.add( result );
+		arbt = new JButton[10];
+		// create buttons
+		for(int i =0;i<10;i++){
+		arbt[i] = new JButton(Integer.toString(i));
+		panel.add(arbt[i]);
+		}
+		div = new JButton( "/" );
+		panel.add( div );
+		mult = new JButton( "*" );
+		panel.add( mult );
+		sub = new JButton( "-" );
+		panel.add( sub );
+		equal = new JButton( "=" );
+		panel.add( equal );
+		add = new JButton( "+" );
+		panel.add( add );
+		clear = new JButton( "c" );
+		panel.add( clear );
+		this.add(panel);
+		//acction listener for button inner class
+		ButtonHandler handler = new ButtonHandler();
+		for(int i=0;i<10;i++){
+			arbt[i].addActionListener(handler);
+			}
+		add.addActionListener( handler );
+		sub.addActionListener( handler );
+		div.addActionListener( handler );
+		mult.addActionListener( handler );
+		equal.addActionListener( handler );
+		result.addActionListener (handler);
+		clear.addActionListener (handler);
+		////////////////////////////////////////////////////
+		this.setDefaultCloseOperation(this.EXIT_ON_CLOSE );
+		setSize( 200, 190 );
+		setVisible( true );
+	}
+		public static void main(String[] args) {
+		new Calculator();
+		}
+
+
+}// class JFram
